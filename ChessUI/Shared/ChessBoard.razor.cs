64de3e0e571d8e654809d888b9f83fc645e7829e
Franco Pinacca <francoo_27@hotmail.com>
2022-09-17 21:37:58 -0300

    Wip ChessBoard Component

---
+++ ChessUI/Shared/ChessBoard.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace ChessUI.Pages.Components
+{
+    public partial class Chessboard
+    {
+    }
+}
+
